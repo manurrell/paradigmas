c95b12e9597d4bb6c7ac98fe50af4ac545e878ee
manuel boorrell <borrellmanuel1@gmail.com>
2023-08-12 18:21:24 -0300

    manu

--- funciones.hs
+++ funciones.hs
@@ -4,7 +4,7 @@ sqr x = x*x
 newP :: Int -> Int -> Point
 newP a b=Poi a b
 distP :: Point -> Point -> Float
-distP (Poi x1 y1) (Poi x2 y2) = sqrt(sqr(fromIntegral(x1-x2)+sqr(fromIntegral(y1-y2))))
+distP (Poi x1 y1) (Poi x2 y2) = sqrt(sqr(fromIntegral(x1-x2))+sqr(fromIntegral(y1-y2)))
 newC :: String -> Point -> City
 nameC :: City -> String
 distanceC :: City -> City -> Float
