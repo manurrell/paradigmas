cbbdf584f2774988e7d0a80df992417308a839b7
myopian1 <leporacelara@gmail.com>
2023-08-12 17:44:32 -0300

    a

--- pruebas.hs
+++ pruebas.hs
@@ -1,5 +1,10 @@
 data Hola = Chau String Int deriving Show
 newH a b = Chau a b
-m=Chau "hoho" 2
+m=Chau "hoho" 2                         --por que
 
 strongH (Chau a b) = a
+
+sqr x = x*x
+
+data Punto = Poi Int Int deriving Show
+
