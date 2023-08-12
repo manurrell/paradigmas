88266ba58c3687501efa43de81615d47f5df0a43
manuel boorrell <borrellmanuel1@gmail.com>
2023-08-12 18:13:14 -0300

    miau

--- funciones.hs
+++ funciones.hs
@@ -1,8 +1,9 @@
-import Point
-import City
+data Point = Poi Int Int deriving (Eq, Show)
+data City = Cit String Point deriving (Eq, Show)
+sqr x = x*x
 newP :: Int -> Int -> Point
 newP a b=Poi a b
-distP :: Punto -> Punto -> Float
+distP :: Point -> Point -> Float
 distP (Poi x1 y1) (Poi x2 y2) = sqrt(sqr(fromIntegral(x1-x2)+sqr(fromIntegral(y1-y2))))
 newC :: String -> Point -> City
 nameC :: City -> String
