9baf3568bf5b1b0076b17861f00b21742ad84990
manuel boorrell <borrellmanuel1@gmail.com>
2023-08-12 19:08:28 -0300

    links

--- funciones.hs
+++ funciones.hs
@@ -1,6 +1,7 @@
 data Point = Poi Int Int deriving (Eq, Show)
 data City = Cit String Point deriving (Eq, Show)
 data Quality = Qua String Int Float deriving (Eq, Show)
+data Link = Lin City City Quality deriving (Eq, Show)
 
 sqr x = x*x
 newP :: Int -> Int -> Point
@@ -21,6 +22,13 @@ delayQ :: Quality -> Float
 newQ tipo cap delay= Qua tipo cap delay
 capacityQ (Qua tipo cap delay)= cap
 delayQ (Qua tipo cap delay)= delay
+
+newL :: City -> City -> Quality -> Link
+newL ciudad1 ciudad2 calidad= Lin ciudad1 ciudad2 calidad
+connectsL :: City -> Link -> Bool
+connectsL c (Lin c1 c2 q) | c==c1 = True | c==c2 = True | otherwise = False
+linksL :: City -> City -> Link -> Bool
+linksL c1 c2 (Lin c3 c4 q) | c1 == c2 = False | ((c1==c3) || (c1==c4)) && ((c2==c3) || (c2==c4)) = True
 
 a= newP 0 0
 b= newP 1 1
