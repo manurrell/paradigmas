c655a59dc511e956e11b18627c892fb823612d1e
manuel boorrell <borrellmanuel1@gmail.com>
2023-08-14 19:21:48 -0300

    a

--- funciones.hs
+++ funciones.hs
@@ -2,6 +2,7 @@ data Point = Poi Int Int deriving (Eq, Show)
 data City = Cit String Point deriving (Eq, Show)
 data Quality = Qua String Int Float deriving (Eq, Show)
 data Link = Lin City City Quality deriving (Eq, Show)
+data Region = Reg [City] [Link] [Tunel]
 
 sqr x = x*x
 newP :: Int -> Int -> Point
@@ -44,6 +45,15 @@ usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese
 usesT link (Tun links)= elem link links 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 delayT (Tun links) =  sum(map delayL links)
+
+-- newR :: Region
+-- foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
+-- linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
+-- tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
+-- connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
+-- linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
+-- delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+-- availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
 
 a= newP 0 0
 b= newP 1 1
