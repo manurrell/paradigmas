46ffbfb5a71c142865ad14ee53f2974c49f15f49
myopian1 <leporacelara@gmail.com>
2023-08-16 10:27:48 -0300

    comentarios_ayuda1

--- funciones.hs
+++ funciones.hs
@@ -8,31 +8,30 @@ distP (Poi x1 y1) (Poi x2 y2) = sqrt(sqr(fromIntegral(x1-x2))+sqr(fromIntegral(y
 data City = Cit String Point deriving (Eq, Show) ---------------------------------------------------
 newC :: String -> Point -> City
 nameC :: City -> String
-distanceC :: City -> City -> Float
+distC :: City -> City -> Float
 newC name location=  Cit name location
 nameC (Cit name location)= name
-distanceC (Cit name location) (Cit name2 location2)= distP location location2
+distanceC (Cit name1 location1) (Cit name2 location2)= distP location1 location2
 
 data Quality = Qua String Int Float deriving (Eq, Show) ----------------------------------------------------
 newQ :: String -> Int -> Float -> Quality
-capacityQ :: Quality -> Int
-delayQ :: Quality -> Float 
 newQ tipo cap delay= Qua tipo cap delay
-capacityQ (Qua tipo cap delay)= cap
-delayQ (Qua tipo cap delay)= delay
+capacityQ :: Quality -> Int
+capacityQ (Qua tipo cap delay)= cap                 --obtener int capacidad
+delayQ :: Quality -> Float
+delayQ (Qua tipo cap delay)= delay                  --obtener float delay
 
 data Link = Lin City City Quality deriving (Eq, Show) ---------------------------------------------------
 newL :: City -> City -> Quality -> Link
 newL ciudad1 ciudad2 calidad= Lin ciudad1 ciudad2 calidad
-connectsL :: City -> Link -> Bool
+connectsL :: City -> Link -> Bool                               --chequear si una ciudad forma parte de un link
 connectsL c (Lin c1 c2 q) | c==c1 = True | c==c2 = True | otherwise = False
-linksL :: City -> City -> Link -> Bool
+linksL :: City -> City -> Link -> Bool                          --chequear si dos ciudades estan linkeadas por 1 link
 linksL c1 c2 (Lin c3 c4 q) | c1 == c2 = False | ((c1==c3) || (c1==c4)) && ((c2==c3) || (c2==c4)) = True
 capacityL :: Link -> Int   
-capacityL(Lin ciudad1 ciudad2 calidad)=capacityQ calidad
+capacityL(Lin ciudad1 ciudad2 calidad)=capacityQ calidad        --obtener int capacidad de la calidad asignada al link
 delayL :: Link -> Float
-delayL(Lin ciudad1 ciudad2 calidad)= delayQ calidad
-
+delayL(Lin ciudad1 ciudad2 calidad)= delayQ calidad             --obtener float delay de la calidad asignada al link
 
 data Tunel = Tun [Link] deriving (Eq, Show) ---------------------------------------------------
 orA :: [Bool] -> Bool
