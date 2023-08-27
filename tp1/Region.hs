7085aa031465688f670329e130d865e9ffbe5ec0
manuel boorrell <borrellmanuel1@gmail.com>
2023-08-26 21:15:23 -0300

    aa

--- tp1/Region.hs
+++ tp1/Region.hs
@@ -13,7 +13,6 @@ linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de l
 linkR (Reg c l t) ciudad1 ciudad2 calidad | orA (map (linksL ciudad1 ciudad2) l) = error "ya existe un link establecido entre las dos ciudades"
                                                             |not(verifyC (Reg c l t) ciudad1 && verifyC (Reg c l t) ciudad2) = error "una o mas de las ciudades no existe en la region"
                                                             |otherwise= Reg c ((newL ciudad1 ciudad2 calidad):l) t
-
 orA :: [Bool] -> Bool
 orA a= foldr (||) False a
 andA :: [Bool] -> Bool
@@ -21,8 +20,6 @@ andA a= foldr (&&) True a
 verifyL :: Region -> [City] -> Bool     
 verifyL (Reg c l t) (x1:(x2:xs)) | xs==[] = orA(map(linksL x1 x2) l) | otherwise = andA(orA(map(linksL x1 x2) l):(verifyL (Reg c l t) (x2:xs)):[])
 
-iflinkreturnlink :: City -> City -> Link -> Link
-iflinkreturnlink c1 c2 l | (linksL c1 c2 l) = l | otherwise = error "no linkea"
 obtenerLinksOrdenados :: [Link] -> [City] -> [Link]
 obtenerLinksOrdenados l (x1:(x2:xs)) | xs==[] = [li | li <- l, linksL x1 x2 li]
                                     | otherwise = [li | li <- l, linksL x1 x2 li] ++ (obtenerLinksOrdenados l (x2:xs))
@@ -32,7 +29,7 @@ verifyC (Reg c l t) ciudad = elem ciudad c
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas 
 linkedR (Reg c l t) ciudad1 ciudad2 = orA(map (linksL ciudad1 ciudad2) l)
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
-delayR region ciudad1 ciudad2 | (verifyC region ciudad1) && (verifyC region ciudad2) = distanceC ciudad1 ciudad2 | otherwise = error"Las ciudades no pertenecen a esta region"
+delayR region ciudad1 ciudad2 | (verifyC region ciudad1) && (verifyC region ciudad2) = distanceC ciudad1 ciudad2 | otherwise = error"Una o mas de las ciudades no pertenecen a esta region"
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
 tunelR (Reg c l t) ciudades |(length ciudades==0 || length ciudades==1) = error"ingrese al menos dos ciudades para construir tunel"
                             |not(verifyL (Reg c l t) ciudades) = error "no existe uno o mas de los links necesarios dentro de la región"
@@ -45,7 +42,7 @@ countA target = foldr (\each fold -> if target == each then fold + 1 else fold) 
 capCheck :: [Link] -> [Tunel] -> Bool      
 capCheck (x1:[]) t = (countA True (map (usesT x1) t)) < capacityL x1
 capCheck (x1:xs) t = andA ((countA True (map (usesT x1) t) < capacityL x1):(capCheck xs t):[])
-
+-- 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel 
 connectedR (Reg ciudades links tuneles) ciudad1 ciudad2= orA(map(connectsT ciudad1 ciudad2)tuneles)
 
@@ -53,7 +50,3 @@ connectedR (Reg ciudades links tuneles) ciudad1 ciudad2= orA(map(connectsT ciuda
 availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades 
 availableCapacityForR (Reg c l t) c1 c2 | verifyL (Reg c l t) [c1, c2] = capacityL(head(obtenerLinksOrdenados l [c1, c2]))
                                         |otherwise= error "no existe un link entre las dos ciudades"
-
-newLinks :: [City] -> Quality -> [Link]         
-newLinks (x1:[]) q= []
-newLinks (x1:(x2:xs)) q= (newL x1 x2 q):(newLinks (x2:xs) q)
--- tp1/escenarios.hs
+++ tp1/escenarios.hs
@@ -18,10 +18,12 @@ lowQuality= newQ "baja calidad" 1 1
 midQuality= newQ "media calidad" 2 0.7
 highQuality= newQ "alta calidad" 5 0.2
 link12= newL ciudad1 ciudad2 highQuality
+link13= newL ciudad1 ciudad3 highQuality
 link23= newL ciudad2 ciudad3 highQuality
 link21= newL ciudad2 ciudad1 highQuality
 link41=newL ciudad4 ciudad1 highQuality
 tunel12= newT [link12]
+tunel13= newT [link13]
 tunel21= newT [link21]
 tunel123= newT  [link12,link23]
 a=newR
@@ -33,7 +35,7 @@ f= linkR e ciudad2 ciudad3 lowQuality
 g= tunelR f [ciudad2, ciudad3]
 i= foundR g ciudad4
 
-verification_list=[linksL ciudad1 ciudad2 link12, linksL ciudad1 ciudad2 link21, distanceC ciudad1 ciudad2 == difP p1 p2, linkedR d ciudad1 ciudad2, link12/=link21]
+verification_list=[linksL ciudad1 ciudad2 link12, linksL ciudad1 ciudad2 link21, distanceC ciudad1 ciudad2 == difP p1 p2, linkedR d ciudad1 ciudad2, link12/=link21, tunel13/=tunel123]
 
 ---------------------- Error Verifications
 testF :: Show a => a -> Bool
