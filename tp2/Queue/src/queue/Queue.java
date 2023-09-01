b3d6d333858706e08684cd0a63978aae64cc6ada
manurrell <borrellmanuel1@gmail.com>
2023-09-01 16:27:59 -0300

    miau

--- tp2/Queue/src/queue/Queue.java
+++ tp2/Queue/src/queue/Queue.java
@@ -1,30 +1,42 @@
 package queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Queue {
-
+	
+	
+  private List<Object>  toDoList = new ArrayList<>();
+  
+  
   public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return true;
+		return this.toDoList.isEmpty();
 	}
 
-	public Queue add( Object  cargo ) {
+  public Queue add( Object  cargo ) {
 		// TODO Auto-generated method stub
+	  this.toDoList.add(0, cargo);
 		return this;
 	}
 
-	public Object take() {
+  public Object take() {
     // TODO Auto-generated method stub
-		return null;
+	  if (this.toDoList.size()==0) {
+		  throw new RuntimeException("Queue is empty");
+	  }
+	  Object cabeza= this.head();
+	  this.toDoList.remove((toDoList.size())-1);
+		return cabeza;
 	}
 
-	public Object head() {
+  public Object head() {
 		// TODO Auto-generated method stub
-    return null;
+    return this.toDoList.get((toDoList.size())-1);
 	}
 
-	public int size() {
+  public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.toDoList.size();
 	}
 
 }
