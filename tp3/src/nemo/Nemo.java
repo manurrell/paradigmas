91f7377d21bf89edfc0e224940161aabd82a2cdf
manurrell <borrellmanuel1@gmail.com>
2023-10-08 18:40:18 -0300

    a

--- tp3/src/nemo/Nemo.java
+++ tp3/src/nemo/Nemo.java
@@ -7,6 +7,13 @@ public class Nemo {
 	private ArrayList<Container> cap_list= new ArrayList<>();
 	private ArrayList<Estados> state_list= new ArrayList<>();
 	private Cardinal aim= new North();
+	
+	public int getCapsuleAmount() {
+		return (cap_list.size()-1);
+	}
+	public Cardinal getAim() {
+		return aim;
+	}
 	public int getX(){
 		return cords.get(0);
 	}
