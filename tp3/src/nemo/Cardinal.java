a55e791fd9586f0b73460120f49b0ce3611c9601
manurrell <borrellmanuel1@gmail.com>
2023-10-08 18:37:23 -0300

    a

--- tp3/src/nemo/Cardinal.java
+++ tp3/src/nemo/Cardinal.java
@@ -1,5 +1,16 @@
 package nemo;
 
 public class Cardinal {
-
+	public  North north() {
+		return new North();
+	}
+	public  South south() {
+		return new South();
+	}
+	public West west() {
+		return new West();
+	}
+	public East east() {
+		return new East();
+	}
 }
--- tp3/src/nemo/Nemo.java
+++ tp3/src/nemo/Nemo.java
@@ -6,6 +6,14 @@ public class Nemo {
 	private ArrayList<Integer> cords= new ArrayList<>();
 	private ArrayList<Container> cap_list= new ArrayList<>();
 	private ArrayList<Estados> state_list= new ArrayList<>();
-	private Cardinal aim= Cardinal.north();
-	
+	private Cardinal aim= new North();
+	public int getX(){
+		return cords.get(0);
+	}
+	public int getY(){
+		return cords.get(1);
+	}
+	public int getZ(){
+		return cords.get(2);
+	}
 }
